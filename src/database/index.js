5f4cd6654fb1402de4b5e116efe906ea62978fd8
lucianopf <luciano.franca@pagar.me>
2022-03-17 21:25:01 -0300

    github actions: setup ci

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,11 @@ const {
 } = process.env
 
 if (NODE_ENV === 'test' || NODE_ENV === 'development') {
+  dynamoose.aws.sdk.config.update({
+    accessKeyId: 'AKID',
+    secretAccessKey: 'SECRET',
+    region: 'us-east-1',
+  })
   dynamoose.aws.ddb.local()
 }
 
